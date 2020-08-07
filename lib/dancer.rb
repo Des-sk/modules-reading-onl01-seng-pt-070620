2ebf18c553467fc119e3daf9002176309e8960b1
Desiree Marquez <marquezdecardona@gmail.com>
2020-08-07 23:34:47 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -5,7 +5,7 @@ require_relative './fancy_dance.rb'
 class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
- end 
+
  
   attr_accessor :name
  
@@ -13,4 +13,4 @@ class Dancer
     @name = name
   end
 
-
+end
